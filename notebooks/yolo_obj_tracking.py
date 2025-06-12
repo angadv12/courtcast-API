0828a7d0b620e01e0465f7254b825c4786f46895
Angad Brar <angadbrar@outlook.com>
2025-06-12 03:06:59 -0400

    ran on 50 epochs, higher 1280 res, 4 batches, new augmentation techniques, put in script instead of notebook for training speed

---
+++ notebooks/yolo_obj_tracking.py
@@ -0,0 +1,58 @@
+if __name__ == "__main__":
+    import os
+    import subprocess
+    import sys
+
+    sys.stdout.flush()
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+    os.environ['PYTHONUNBUFFERED'] = '1'
+
+    # Get GPU information
+    try:
+        result = subprocess.run(['nvidia-smi'], capture_output=True, text=True, check=True)
+        print(result.stdout)
+        print("=" * 50)
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("nvidia-smi not available or no NVIDIA GPU detected")
+
+    import os
+    HOME = os.getcwd()
+    print(HOME)
+
+    import ultralytics
+    print(ultralytics.checks())
+    from ultralytics import YOLO
+    from PIL import Image
+    import requests
+
+    from dotenv import load_dotenv
+    from roboflow import Roboflow
+    load_dotenv()
+    ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API_KEY")
+    rf = Roboflow(api_key=ROBOFLOW_API_KEY)
+
+    workspace = rf.workspace("angadv12")
+    project = workspace.project("courtcast")
+    version = project.version(1)
+    dataset = version.download("yolov11")
+
+    model = YOLO('yolo11s.pt')
+    results = model.train(
+        data=r'C:\Users\Angad Brar\Desktop\courtcast-API\notebooks\courtcast-1\data.yaml',
+        epochs=50,
+        imgsz=1280,
+        batch=4,
+        patience=15,
+        lr0=0.001,
+        lrf=0.01,
+        
+        warmup_epochs=3,
+        warmup_bias_lr=0.1,
+        warmup_momentum=0.8,
+        
+        degrees=5,
+        translate=0.1,
+        mosaic=0.5,
+        plots=True,
+        verbose=True,
+    )
